7cd35948c77ae8f89d4617427e2f9e61eba0c6ae
mohsen <mohsenmanoochehr@gmail.com>
2025-05-20 09:46:42 +0330

    site header small change

--- src/layouts/site/Header.js
+++ src/layouts/site/Header.js
@@ -9,11 +9,21 @@ function Header() {
         width={160}
         height={160}
         alt="pumpkin logo"
-        className="w-30 h-auto filter drop-shadow-white"
+        className="w-30 h-auto"
+        style={{
+          filter: "drop-shadow(0 0 5px rgba(200, 200, 200, 0.85))",
+        }}
       />
-      <nav className="flex items-center">
-        <Link href="/sign-in">Sign In</Link>
-        <Link href="/sign-up">Sign Up</Link>
+      <nav className="flex items-center gap-2">
+        <Link
+          href="/sign-in"
+          className="py-3 px-2 text-white text-3xl rounded border-l-indigo-800"
+        >
+          Sign In
+        </Link>
+        <Link href="/sign-up" className="py-3 px-2 text-white text-3xl">
+          Sign Up
+        </Link>
       </nav>
     </header>
   );
